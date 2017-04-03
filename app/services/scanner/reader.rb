764d7fd9cc80e667c7dd6b64a97ffab5ef8251c8
briley <brian.riley@ucop.edu>
2017-04-03 12:38:27 -0700

    added debug helpers

--- app/services/scanner/reader.rb
+++ app/services/scanner/reader.rb
@@ -24,6 +24,8 @@ module Scanner
         
         parsed = parser.parse(feed, response.body)
         
+puts "HERE I AM"
+        
         unless parsed.nil?
           parsed.items.each do |item|
             unless item.nil?
