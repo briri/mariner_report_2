f5a4c6d50f9c5a15bfefd1ddf4575a336503a479
Brian Riley <riley.bri@gmail.com>
2017-04-09 21:54:49 -0700

    updated category-article query and placed a limit of 59 articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,8 @@ class CategoriesController < ApplicationController
   # GET /categories/[:slug]
   # ----------------------------------------------------
   def show
-    @category = Category.find_by(slug: params[:id])
+    @category = Category.eager_load(:articles).
+        find_by('articles.slug = ? AND active = true', params[:id])[0..59]
   end
   
 end
